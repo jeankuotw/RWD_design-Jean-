cfb71714193f245c70954aba29b883713ff75a4a
jeankuotw <19840920kuo@yahoo.com.tw>
2019-12-11 20:35:28 +0800

    更新header、product頁面RWD

--- js/all.js
+++ js/all.js
@@ -38,14 +38,13 @@ $(function(){
 		$("body,html").animate({scrollTop:0},2500);
 	})
 
-// 監測hidemenu click動作,會出現直排的menu
-	$(".hidemenu").on('click', function(e){
+// 監測漢堡選單hidemenu click動作,會出現直排的menu
+	$(".hamburgermenu").on('click', function(e){
 		// 取消<a>預設超連結功能
 		e.preventDefault();
 		// 將.nav套上class後,menu即可開合
 		$(".nav").toggleClass('menu-show');
-	})
-
+	});
 
 
 
